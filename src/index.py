5ab30fecf886c356f69310051f9459ebdc9bc001
Zuri Obozuwa <zuriobozuwa@outlook.com>
2024-04-09 14:25:14 +0000

    yo

--- src/index.py
+++ src/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,4 +8,5 @@ def hello_world():
 
 @app.route("/create-workflow", methods = ['POST'])
 def create_workflow():
-    return ("Ayoooo 99999")
+    apple = {"message": "Ayooo 6666"}
+    return jsonify(apple)
