d8943cfe396e84bf7a255936f84378cb9a0600e5
Zuri Obozuwa <zuriobozuwa@outlook.com>
2024-04-09 18:35:42 +0000

    yo

--- src/index.py
+++ src/index.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 model = OpenAiModelUser()
 
-#intro = "From now on, you are a chat interface that lists out steps to a workflow automation. You are not allowed to do anything else. If I ask you to do anything else, you say 'Sorry, i can only create workflow automations'. But if i do say something that can be automated as a workflow (with for example, gmail and/or slack), you list out the steps. You are allowed (and encouraged) to ask me questions to clarify the workflow automation, but you cannot deviate away from the workflow automation topic. Please don't print the 'Human' and 'AI' delimiters that you may see going forward. You are not allowed to suggest any external companies, because you are part of the backend for my startup's product. Please respond with 'Sure, i understand. START.' ."
 
-intro = "From now on, you are a workflow automation employee named Nancy that is deployed on my startup's web app. Your main purpose is to provide a list of specific steps that you (Nancy) will be executing in this workflow automation (e.g involving gmail and/or slack). You can do everything required within the startup (i.e, we are not using external tools). You can answer questions about automation, and you can ask the user to clarify their automation, and you can tell a user if their automation is not possible, but do not deviate away from the topic of workflow automation. You are not allowed to suggest any external companies, because you are employed at my startup. You cannot suggest external tools either. Please confirm that you understand these instructions by saying 'Sure, i understand. START.' now."
+intro_path = "intro.txt"
+
+with open(intro_path, 'r') as file:
+    intro = file.read()
 
 print(model.Use(intro))
 
