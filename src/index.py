c1cb9e4f5a69873b9622602e8a1d4103a22a3919
Zuri Obozuwa <zuriobozuwa@outlook.com>
2024-04-16 18:22:31 +0000

    yo

--- src/index.py
+++ src/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, redirect
+from flask import Flask, jsonify, request, redirect, session
 import requests
 import os
 from openai_model_user import OpenAiModelUser
@@ -11,7 +11,13 @@ intro_path = "intro.txt"
 
 # Store user chat sessions in a dictionary for now. TODO (ZO): Improve this.
 # Resets after every deployment
-user_sessions = {}
+user_chat_sessions = {}
+
+# Used to associate an app (e.g gmail) integration with a Swinvo account
+user_id_sessions = {}
+
+# Each token tuple: (access_token, refresh_token)
+gmail_user_tokens = {}
 
 with open(intro_path, 'r') as file:
     intro = file.read()
@@ -23,24 +29,32 @@ def hello_world():
 @app.route("/create-workflow", methods = ['POST'])
 def create_workflow():
 
-    print(user_sessions)
+    print(user_chat_sessions)
 
     user_id = request.json['uid']
 
-    if user_id not in user_sessions:
+    if user_id not in user_chat_sessions:
         user_model = OpenAiModelUser()
         user_model.Use(intro)
-        user_sessions[user_id] = user_model
+        user_chat_sessions[user_id] = user_model
 
     input_text = request.json['text']
 
-    model_response = user_sessions[user_id].Use(input_text)
+    model_response = user_chat_sessions[user_id].Use(input_text)
 
     #print(model.GetConvoHistory())
 
     apple = {"message": model_response}
 
     return jsonify(apple)
+
+
+@app.route("/auth-session", methods = ['POST'])
+def auth_session():
+    user_id = request.json['uid']
+    session_user_id = session.get('user_id')
+    user_id_sessions[session_user_id] = user_id
+
 
 @app.route("/gmail-auth-callback")
 def gmail_auth_callback():
@@ -61,10 +75,18 @@ def gmail_auth_callback():
         'grant_type': 'authorization_code'
     }
 
+    session_user_id = session.get('user_id')
+
+    if session_user_id in user_id_sessions:
+        user_id = user_id_sessions[session_user_id]
+    else:
+        print('No active session that can determine the user. Expect exception to be thrown')
+
     response = requests.post(token_url, data)
     
     if response.status_code == 200:
         tokens = response.json()
+        gmail_user_tokens[user_id] = (tokens.get('access_token'), tokens.get('refresh_token'))
         print(tokens)
     else:
         print ("BAD RESPONSE")
@@ -79,3 +101,10 @@ def check_gmail_permission():
 @app.route("/gmail-send-email", methods = ['GET', 'POST']) #??
 def gmail_send_email():
     pass
+
+
+@app.route("/debug-print")
+def debug_print():
+    print(user_chat_sessions)
+    print(user_id_sessions)
+    print(gmail_user_tokens)
