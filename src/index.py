5703f98771325d7cfa089eb8383fd068aac7b986
Zuri Obozuwa <zuriobozuwa@outlook.com>
2024-05-05 11:45:17 +0000

    yo

--- src/index.py
+++ src/index.py
@@ -436,3 +436,28 @@ def stripe_create_checkout_session():
     except Exception as e:
         print("stripe error: ", str(e))
         return flask.jsonify(error=str(e)), 403
+
+@app.route("/stripe-webhook", methods = ['POST'])
+def stripe_webhook()
+    endpoint_secret = ""
+    print("stripe webhook")
+
+    payload = request.data
+    sig_header = request.headers.get('Stripe-Signature')
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return 'Invalid payload', 400
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return 'Invalid signature', 400
+
+    print(event)
+
+    # Handle event with mongo here
+
+    return jsonify(success=True), 200
